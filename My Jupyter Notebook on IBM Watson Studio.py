b6dc48121747a7eb59ac73f4f56aaf1faf60958f
AnassTA <98308552+AnassTA@users.noreply.github.com>
2022-08-22 18:16:37 +0100

    Add files via upload

---
+++ My Jupyter Notebook on IBM Watson Studio.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Anass TAZI**
+# 
+# I'm a Procurement Manager and I want to switch my career to Data Science
+
+# *I am interested in data science because I like to play with Data and understand its patterns and also I like creating predective models*
+
+# ### The following code count from 1 to 10
+
+# In[2]:
+
+
+for i in range(1,11):
+    print(i)
+
+
+# ***
+# 
+# This steps must be respected
+# 
+# ****
+# 
+# * Upload the Data
+# * Explore the Data
+# * Clean the Data
+# * Visualize the Data
+#     * Draw charts
+# 
+# <a href="http://coursera.org" target="_blank">coursea</a>
+
+# In[ ]:
+
+
+
+
